25a48cd77cfaf5a1a193cfe5bc23f2bf6e1397a1
Imnonumbe <imnonumbe@gmail.com>
2025-06-21 19:12:21 +0200

    Add files via upload

--- questions.js
+++ questions.js
@@ -1,6 +1,6 @@
 const QUIZDATA = { title: '10 Pytań do Marzeń', questions: [
     { title: '1. Czy masz jakieś marzenia, które naprawdę Cię poruszają?', ans1: 'TAK', ans2: 'NIE', subq2: {
-        title: 'A czy chciałabyś je mieć lub je odnaleźć?', ans1: 'TAK', ans2: 'NIE', subq1: {
+        title: 'A czy uważasz, że warto je mieć?', ans1: 'TAK', ans2: 'NIE', subq1: {
             title: 'A teraz wyobraź sobie, że je masz', ans1: '\u2192'
         }, subq2: {
             title: 'Dziękuję Ci za szczerość. Gdyby coś się w Tobie kiedyś obudziło, będę \ud83d\udc9b'
@@ -10,14 +10,14 @@ const QUIZDATA = { title: '10 Pytań do Marzeń', questions: [
         title: 'Wiedz, że możesz poczuć większą wiarę w siebie', ans1: '\u2192'
     } },
     { title: '3. Czy masz już jakiś konkretny plan lub sposób na ich realizację?', ans1: 'TAK', ans2: 'NIE', subq1: {
-        title: 'A czy jesteś pewna, że zadziała?', ans1: 'TAK', ans2: 'NIE / NIE\xa0WIEM'
+        title: 'A czy masz pewność, że zadziała?', ans1: 'TAK', ans2: 'NIE / NIE\xa0WIEM'
     } },
-    { title: '4. Rozumiem. A czy chciałabyś poznać sposób, który działa dla mnie i moich partnerek?', ans1: 'TAK', ans2: 'NIE', subq2: {
-        title: 'OK, to też jest decyzja. Gdybyś kiedyś zmieniła zdanie, jestem tu \ud83d\udc9b'
+    { title: '4. Rozumiem. A czy chcesz poznać sposób, który działa dla mnie i dla naszego zespołu?', ans1: 'TAK', ans2: 'NIE', subq2: {
+        title: 'OK, to też jest decyzja. Jeśli kiedyś zmienisz zdanie, jestem tu \ud83d\udc9b'
     } },
-    { title: '5. Super! Ile byłabyś gotowa zainwestować w spełnienie swojego marzenia?', ans1: 'więcej\u2009niż pensja', ans2: 'mniej niż pensja' },
+    { title: '5. Super! Czy warto zainwestować więcej niż jedną pensję w spełnienie swojego marzenia?', ans1: 'TAK', ans2: 'NIE' },
     { title: '6. A teraz wyobraź sobie, że możesz zacząć od 1500 zł. Czy to dla Ciebie możliwe?', ans1: 'TAK', ans2: 'NIE / NIE\xa0WIEM', subq2: {
-        title: 'A czy chciałabyś, aby to było dla Ciebie możliwe?', ans1: 'TAK', ans2: 'NIE', subq2: {
+        title: 'A czy chciał(a)byś, aby to było dla Ciebie możliwe?', ans1: 'TAK', ans2: 'NIE', subq2: {
             title: 'Szanuję twoją decyzję. Jeżeli kiedykolwiek zmienisz zdanie, pamiętaj \u2013 jestem \ud83d\udc9b'
         }
     } },
@@ -27,12 +27,12 @@ const QUIZDATA = { title: '10 Pytań do Marzeń', questions: [
     }, subq2: {
         title: 'Dziękuję za szczerość. Zawsze możesz wrócić, gdyby coś się zmieniło \ud83d\udc9b'
     } },
-    { title: '9. Czy jesteś gotowa uczyć się nowego myślenia, działania i świadomie budować drogę do marzeń?', ans1: 'TAK', ans2: 'NIE', subq2: {
+    { title: '9. Czy masz gotowość do uczenia się nowego myślenia, działania i świadomego budowania drogi do marzeń?', ans1: 'TAK', ans2: 'NIE', subq2: {
         title: 'Rozumiem, że tak czasami może być. Jeśli potrzebujesz rozmowy, daj znać \ud83d\udc9b'
     } },
     { title: '10. To co \u2013 robimy to razem? \ud83d\udc9b', ans1: 'TAK', ans2: 'NIE / NIE\xa0WIEM', subq1: {
         title: 'Witamy na pokładzie! Podejmujesz decyzję, która może zmienić wszystko \ud83d\udc9b'
     }, subq2: {
-        title: 'Co mogłoby pomóc Ci powiedzieć \u201etak\u201d? Może porozmawiamy? \ud83d\udc9b'
+        title: 'Co mogłoby pomóc Ci powiedzieć \u201etak\u201d? Możemy porozmawiać, jeśli potrzebujesz \ud83d\udc9b'
     } }
 ] };
